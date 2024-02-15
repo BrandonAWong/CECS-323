d3a29fe4986d0052c1d733976905fc6f36cd674c
BrandonAWong <brandonwong2004@outlook.com>
2024-02-14 19:54:37 -0800

    add init helper and force all section columns as mandatory

--- OneToMany/Section.py
+++ OneToMany/Section.py
@@ -17,19 +17,19 @@ if introspection_type == START_OVER or introspection_type == REUSE_NO_INTROSPECT
         course: Mapped["Course"] = relationship(back_populates="sections")
 
         departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation',
-                                                  String(10), primary_key=True)
+                                                  String(10), nullable=False, primary_key=True)
         courseNumber: Mapped[int] = mapped_column('course_number', Integer,
                                                   nullable=False, primary_key=True)
         sectionNumber: Mapped[int] = mapped_column('section_number', Integer,
-                                                  primary_key=True)
+                                                  nullable=False, primary_key=True)
         semester: Mapped[str] = mapped_column('semester', String(10), nullable=False,
                                                   primary_key=True)
         sectionYear: Mapped[int] = mapped_column('section_year', Integer, nullable=False,
                                                   primary_key=True)
         building: Mapped[str] = mapped_column('building', String(6), nullable=False)
         room: Mapped[int] = mapped_column('room', Integer, nullable=False)
-        schedule: Mapped[str] = mapped_column('schedule', String(6))
-        startTime: Mapped[Time] = mapped_column('start_time', Time)
+        schedule: Mapped[str] = mapped_column('schedule', String(6), nullable=False)
+        startTime: Mapped[Time] = mapped_column('start_time', Time, nullable=False)
         instructor: Mapped[str] = mapped_column('instructor', String(80), nullable=False)
 
         __table_args__ = (UniqueConstraint("section_year", "semester", "schedule", "start_time", 
@@ -39,6 +39,10 @@ if introspection_type == START_OVER or introspection_type == REUSE_NO_INTROSPECT
                           ForeignKeyConstraint([departmentAbbreviation, courseNumber],
                                                [Course.departmentAbbreviation, Course.courseNumber]))
 
+        def __init__(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,
+             building: str, room: int, schedule: str, startTime: Time, instructor: str):
+            self.init_helper(course, sectionNumber, semester, sectionYear, building, room, schedule, startTime, instructor)
+
 elif introspection_type == INTROSPECT_TABLES:
     class Section(Base):
         __table__ = Table("sections", Base.metadata, autoload_with=engine)
@@ -46,7 +50,13 @@ elif introspection_type == INTROSPECT_TABLES:
         courseNumber: Mapped[int] = column_property(__table__.c.course_number)
         course: Mapped["Course"] = relationship(back_populates="sections")
 
-def __init__(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,
+        def __init__(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,
+             building: str, room: int, schedule: str, startTime: Time, instructor: str):
+            self.init_helper(course, sectionNumber, semester, sectionYear, building, room, schedule, startTime, instructor)
+    
+
+# init helper
+def init_helper(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,
              building: str, room: int, schedule: str, startTime: Time, instructor: str):
     self.departmentAbbreviation = course.departmentAbbreviation
     self.course = course
@@ -67,6 +77,6 @@ def __str__(self):
             f"Instructor: {self.instructor} Schedule: {self.schedule} Start time: {self.startTime}\n"
             f"Building: {self.building} Room: {self.room}")
 
-setattr(Section, '__init__', __init__)
+setattr(Section, 'init_helper', init_helper)
 setattr(Section, '__str__', __str__)
 
